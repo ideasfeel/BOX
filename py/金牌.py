bb51bbb0973602b8c2ada745bef3c3ce57f903de
ideasfeel <155930240+ideasfeel@users.noreply.github.com>
2025-11-17 15:38:23 +0800

    Update 金牌.py

--- py/金牌.py
+++ py/金牌.py
@@ -106,7 +106,7 @@ class Spider(Spider):
     def detailContent(self, ids):
         data=self.fetch(f"{self.host}/api/mw-movie/anonymous/video/detail?id={ids[0]}",headers=self.getheaders({'id':ids[0]})).json()
         vod=self.getvod([data['data']])[0]
-        vod['vod_play_from']='金牌播放器'
+        vod['vod_play_from']='金牌影院'
         vod['vod_play_url'] = '#'.join(
             f"{i['name'] if len(vod['episodelist']) > 1 else vod['vod_name']}${ids[0]}@@{i['nid']}" for i in
             vod['episodelist'])
